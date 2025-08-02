1067b9770773668c4a7130eb9ecf07bbf3704f8b
Nite2005 <kumarnitesh140905@gmail.com>
2025-08-02 18:15:10 +0530

    add performance test

--- tests/test_model.py
+++ tests/test_model.py
@@ -3,7 +3,7 @@ import mlflow
 import os
 import pickle
 import pandas as pd
-
+from sklearn.metrics import accuracy_score,precision_score,recall_score,f1_score
 
 class TestModelLoading(unittest.TestCase):
 
@@ -50,7 +50,32 @@ class TestModelLoading(unittest.TestCase):
         self.assertEqual(input_df.shape[1], len(self.vectorizer.get_feature_names_out()))
         self.assertEqual(len(prediction), input_df.shape[0])
         self.assertEqual(len(prediction.shape), 1)
+    
+    def test_model_perfomance(self):
+        #Extract features and labels from holdout test data
+        X_holdout = self.holdout_data.iloc[:,0:-1]
+        y_holdout = self.holdout_data.iloc[:,-1]
 
+        #Predict using the new model
+        y_pred_new = self.new_model.predict(X_holdout)
+
+        #calculate performance metrics for the new model
+        accuracy_new = accuracy_score(y_holdout,y_pred_new)
+        precission_new = precision_score(y_holdout,y_pred_new)
+        recall_new = recall_score(y_holdout,y_pred_new)
+        f1_new = f1_score(y_holdout,y_pred_new)
+
+        #define expected thresholds for the performance metrics
+        expected_accuracy = 0.70
+        expected_precision = 0.70
+        expected_recall = 0.70
+        expected_f1 = 0.70
+
+        #Assert that the new model meets the performance thresholds
+        self.assertGreaterEqual(accuracy_new,expected_accuracy,f'Accuracy should be at least {expected_accuracy}')
+        self.assertGreaterEqual(precission_new,expected_precision,f'Precision should be at least {expected_precision}')
+        self.assertGreaterEqual(recall_new,expected_recall,f'Recall should be at least {expected_recall}')
+        self.assertGreaterEqual(f1_new,expected_f1,f'F1 score should be at least {expected_f1}')
 
 
 
